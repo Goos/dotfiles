f5314699231c854f11f7f2e970e36fae8ca6afb7
Robin Goos <mail@robingoos.com>
2026-06-07 20:28:01 -0700

    Remove old config structure in favor of stow packages

--- .config/nvim/lua/config/options.lua
+++
@@ -1,8 +0,0 @@
-vim.wo.number = true
-vim.o.splitright = true
-vim.o.shiftwidth = 2
-
--- Searching
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
--- .config/nvim/lua/plugins/smear-cursor.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "sphamba/smear-cursor.nvim",
-  opts = {
-    cursor_color = "#D8DEE9",
-    normal_bg = "#2E3440",
-  },
-}
